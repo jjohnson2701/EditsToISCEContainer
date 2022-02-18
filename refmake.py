8e1fe2a4656b6a38a2ba93b2a57f84f32ada45b6
Joel Johnson <71100721+jjohnson2701@users.noreply.github.com>
2022-02-18 14:04:24 -0700

    Update refmake.py

--- refmake.py
+++ refmake.py
@@ -1,15 +1,16 @@
 import numpy as np
 import argparse
 
-# #get directory information.
-# def getparser():
-#     # Parser to submit inputs for scripts. See Jul 27 Email from Jasmine
-#     parser = argparse.ArgumentParser(description="Input list of pairs")
-#     parser.add_argument('pairlist', type=str, help='home directory holding date pair folders. For example, add CameronP78list.txt after calling this script')
-#     return parser
-# parser = getparser()
-# args = parser.parse_args()
-# a= args.pairlist
+# Example of LagosDesc.txt below: (May include 100's of scenes)
+# S1A_IW_SLC__1SDV_20180313T052943_20180313T053012_020992_0240A7_005E
+# S1A_IW_SLC__1SDV_20180325T052943_20180325T053013_021167_024636_8444
+# S1A_IW_SLC__1SDV_20180406T052943_20180406T053013_021342_024BB1_0382
+# S1A_IW_SLC__1SDV_20180418T052944_20180418T053013_021517_025130_BF34
+# S1A_IW_SLC__1SDV_20180430T052944_20180430T053014_021692_0256A8_CF47
+# S1A_IW_SLC__1SDV_20180512T052945_20180512T053015_021867_025C3F_40B0
+# S1A_IW_SLC__1SDV_20180524T052945_20180524T053015_022042_0261CD_9A5C
+# S1A_IW_SLC__1SDV_20180605T052946_20180605T053016_022217_026756_8F57
+
 
 
 a = np.loadtxt('LagosDesc.txt', dtype=str)
@@ -36,9 +37,9 @@ np.savetxt("LagosDescRef4.txt", ref, fmt="%s", delimiter=' ')
 np.savetxt("LagosDescSec4.txt", sec, fmt="%s", delimiter=' ')
 np.savetxt("LagosDescList.txt", pairlist, fmt="%s", delimiter=' ')
 
-#Then I save the Ref2 and Sec2 to the same location as the .sh file, and load them in the following way in the ChileArray.sh script:
-# REF_GRANULE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ChileP62Ref2.txt)
-# SND_GRANULE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" ChileP62Sec2.txt)
+#Then I save the Ref2 and Sec2 to the same location as the .sh file, and load them in the following way in the LagosArray.sh script:
+# REF_GRANULE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" LagosDescRef4.txt)
+# SND_GRANULE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" LagosDescSec4.txt)
 
 
 print("Pair list has " + str(len(pairlist)) + " elements.")
